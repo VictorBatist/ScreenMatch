a4269d30f827d1855f8298241f978d14cc0149dd
Victor Batista <vbatista.dev@gmail.com>
2025-04-11 09:32:51 -0300

    Adicionando modificadores de acesso e organização de pastas

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import victor.screenmatch.models.Filme;
 
 public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.nome = "O Poderoso Chefão";
+        meuFilme.anoDeLancamento = 1978;
+        meuFilme.duracaoEmMinutos = 180;
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(5);
+        meuFilme.avalia(8);
+        meuFilme.avalia(10);
+
+        //System.out.println(meuFilme.somaDasAvaliacoes);
+        System.out.println(meuFilme.getTotalDeAvaliacoes());
+        System.out.println(meuFilme.pegaMedia());
+
+    }
 }
