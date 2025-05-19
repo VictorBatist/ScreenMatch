af13bf5003a1da4f1e58f909f6c395b99e365042
Victor Batista <vbatista.dev@gmail.com>
2025-05-19 09:49:19 -0300

    Tratando e asxceções

--- src/victor/screenmatch/view/Buscas.java
+++ src/victor/screenmatch/view/Buscas.java
@@ -44,10 +44,15 @@ public class Buscas {
         TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
         System.out.println(meuTituloOmdb);
 
-        Titulo meuTitulo = new Titulo(meuTituloOmdb);
-        System.out.println("\nUm titulo já convertido:");
-        System.out.println(meuTitulo);
+        try{
+            Titulo meuTitulo = new Titulo(meuTituloOmdb);
+            System.out.println("Um titulo já convertido:");
+            System.out.println(meuTitulo);
+        }catch (NumberFormatException e){
+            System.out.println("Aconteceu um erro:");
+            System.out.println(e.getMessage());
+        }
 
-
+        System.out.println("O programa finalizou corretamente!");
     }
 }
